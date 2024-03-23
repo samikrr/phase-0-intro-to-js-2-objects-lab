492eb7d0f33ca5c6c2ac4c306df1863ac7c66fd2
Samuel Kirori <samuelkirori300@gmail.com>
2024-03-23 19:31:52 +0300

    Complete Lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+const employee={
+    Name:"Samuel",
+    Streetaddress: 929-642};
+     function updateEmployeeWithKeyAndValue(employee, key, value){
+        return {
+            ...employee,
+            [key]:value,
+        }
+     }
+
+     function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+        employee[key]=value;
+        return employee;
+     }
+     function deleteFromEmployeeByKey(employee, key){
+        const employee1={
+            ...employee1,
+            key:key
+        }
+        return employee1
+     }
+     function destructivelyDeleteFromEmployeeByKey(employee, key){
+        employee[key]=undefined;
+        return employee;
+     }
